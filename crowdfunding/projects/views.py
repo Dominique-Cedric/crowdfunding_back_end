83959685feff0f93c9a6fa54ffe0a470c4948bff
Dominique Cedric <dominique.cedric@shecodes.com.au>
2024-09-30 21:03:13 +1000

    Wrote our first django views

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -14,6 +14,19 @@ class ProjectList(APIView):
         projects = Project.objects.all()
         serializer = ProjectSerializer(projects, many=True)
         return Response(serializer.data)
+    
+    def post(self, request):
+       serializer = ProjectSerializer(data=request.data)
+       if serializer.is_valid():
+           serializer.save()
+           return Response(
+               serializer.data,
+               status=status.HTTP_201_CREATED
+           )
+       return Response(
+           serializer.errors,
+           status=status.HTTP_400_BAD_REQUEST
+       )
 
 class ProjectDetail(APIView):
     def get_object(self, pk):
